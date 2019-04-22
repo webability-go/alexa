24b96f81add1d34c15b35cd98da0d11cb2367525
metalwolf <phil@thomassigny.org>
2019-04-22 10:34:10 -0500

    patch v0.1.0

--- dynamodb.go
+++ dynamodb.go
@@ -1,6 +1,7 @@
 package alexa
 
 import (
+  "github.com/webability-go/alexa/request"
   "github.com/webability-go/alexa/dynamodb"
 )
 
@@ -15,4 +16,14 @@ func WithTableName(name string) {
 
 func WithAutoCreateTable(auto bool) {
   dynamodb.DDB_autocreate = auto
+}
+
+func SavePersistentAttributes(req request.AlexaRequest, data interface{}) error {
+  id := req.GetSessionId()
+  dynamodb.Upsert(id, data)
+  return nil
+}
+
+func LoadPersistentAttributes(req request.AlexaRequest) (interface{}, error) {
+  return nil, nil
 }
