4feef96165953605f567d70e5c23b728f5f49aa6
metalwolf <phil@thomassigny.org>
2020-02-25 20:10:56 -0600

    patch v0.4.0

--- alexa.go
+++ alexa.go
@@ -1,45 +1,51 @@
+// Copyright Philippe Thomassigny 2018-2020.
+// Use of this source code is governed by a MIT licence.
+// license that can be found in the LICENSE file.
+
+// Package alexa is a framework ready-to-go to build Apps for Lambda servers for Amazon Alexa
+//
 package alexa
 
 import (
-  "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambda"
 
-  "github.com/webability-go/alexa/dynamodb"
-  "github.com/webability-go/alexa/request"
+	"github.com/webability-go/alexa/dynamodb"
+	"github.com/webability-go/alexa/request"
 )
 
-const VERSION = "0.3.2"
+const VERSION = "0.4.0"
 
 var DEVEL = false
 var ERRORCAPTURE = true
 
 /*
   Alexa Lamdba Library Manager for GO
-  This is an implementation for a Full functional lamdba function for Alexa. 
+  This is an implementation for a Full functional lamdba function for Alexa.
   Build the skill overloading the default event handlers and adding your own event handlers
-  
+
   Log:
   2019-03-27: Phil, Creation
   2019-04-01: Phil, Add Dynamodb support
 */
 
 func init() {
-  BuildDefaultMap()
+	BuildDefaultMap()
 }
 
 // Anything we need to make alexa work
 func Start() error {
-  if dynamodb.DDB_tablename != "" {
-    err := dynamodb.StartDynamoTable()
-    if err != nil {
-      return err
-    }
-  }
-  lambda.Start(DefaultHandler)
-  return nil
+	if dynamodb.DDB_tablename != "" {
+		err := dynamodb.StartDynamoTable()
+		if err != nil {
+			return err
+		}
+	}
+	lambda.Start(DefaultHandler)
+	return nil
 }
 
 func SetErrorCapture(status bool) {
-  ERRORCAPTURE = status
+	ERRORCAPTURE = status
 }
 
 /* ==========================================================================
@@ -47,7 +53,5 @@ func SetErrorCapture(status bool) {
    ==========================================================================*/
 
 func SetSessionUnmarshalerHandler(unmarshaler func(data []byte, s *request.Session) error) {
-  request.SessionUnmarshalerHandler = unmarshaler
+	request.SessionUnmarshalerHandler = unmarshaler
 }
-
-
